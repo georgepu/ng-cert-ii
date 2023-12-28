4cd020d5264a71dee61c6b649ea691d34c52576b
George Pu <george@george-lenovo>
2023-12-27 19:20:07 -0500

    add 2nd build

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-S42ITGCF.mjs";import"./chunk-TFEETNUZ.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-U2EW7LK2.mjs";import"./chunk-ESWG22TQ.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
